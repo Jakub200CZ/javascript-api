c25bb5d8358f67cb2f41d027081af9203c195c5e
Jakub Sedláček <kubka.sedlacek04@gmail.com>
2022-01-29 00:47:06 +0100

    Add GET and POST

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ app.listen(
     PORT,
     () => console.log(`It's alive on https://localhost:${PORT}`)
 )
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: '👕',
+        size: 'large'
+    })
+});
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!' })
+    }
+
+    res.send({
+        tshirt: `👕 with your ${logo} and ID of ${id}`,
+    });
+});
